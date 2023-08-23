60076912ed426ec31774fe2a4f93bd8fece8529e
Amrit Acharya <66554397+Iamhazard@users.noreply.github.com>
2023-08-23 21:42:32 +0545

    Add files via upload

---
+++ myservlets/src/java/Cookies/SecondServlet.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Cookies;
+import javaX.servlet http *;
+import javaX.servlet *;
+import java.io*;
+
+
+public class SecondServlet extends HttpServlet{
+    
+    
+    
+    
+    
+}
